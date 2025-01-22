ad8322abb90a7371170381bbb03031abdbc4d8e1
angel.chiveg <angel.chiveg@educa.jcyl.es>
2025-01-22 14:19:39 +0100

    Intentar usar Applocalizations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:proyecto_flutter/core/presentation/go_router_config.dart';
 import 'package:proyecto_flutter/core/presentation/theme/theme_constants.dart';
 import 'package:proyecto_flutter/film/presentation/view_model/film_view_model.dart';
 import 'package:proyecto_flutter/list/presentation/view_model/list_view_model.dart';
 import 'package:proyecto_flutter/profile/presentation/view_model/profile_view_model.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 // Si se quieren manejar los snackbar de manera global
 //final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
@@ -37,6 +39,8 @@ class _AppState extends State<App> {
     return MaterialApp.router(
       //scaffoldMessengerKey: scaffoldKey,
       title: 'Filmoteca',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: themeMode,

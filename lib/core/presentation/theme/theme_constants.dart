41ba6fad36da205f650379a51e6ab3c25bd4aad4
angel.chiveg <angel.chiveg@educa.jcyl.es>
2025-01-22 13:07:59 +0100

    Declarar versión de provider. Modificar el estilo del snackbar para que

--- lib/core/presentation/theme/theme_constants.dart
+++ lib/core/presentation/theme/theme_constants.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.light,
+  snackBarTheme: SnackBarThemeData(
+    width: 300,
+    behavior: SnackBarBehavior.floating,
+    showCloseIcon: true,
+  ),
 );
 
 ThemeData darkTheme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.dark,
+  snackBarTheme: SnackBarThemeData(
+    width: 300,
+    behavior: SnackBarBehavior.floating,
+    showCloseIcon: true,
+  ),
 );
